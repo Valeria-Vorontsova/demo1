f5cdcddc714d1ca1edb79ca0445724ad7d047b39
Валерия Воронцова <leraq8630@gmail.com>
2026-02-13 15:46:05 +0500

    list method

--- demo/src/main/java/com/example/demo/controller/PostsViewController.java
+++ demo/src/main/java/com/example/demo/controller/PostsViewController.java
@@ -1,17 +1,17 @@
 package com.example.demo.controller;
 
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
 
 @Controller
 public class PostsViewController {
-    @ResponseBody
+
     @RequestMapping(path = "/", method = RequestMethod.GET)
-    public String list() {
-        return "Здесь будет главная страница";
+    public String list(Model model) {
+        model.addAttribute("appName", "Моё супер приложение");
+        return "list";
     }
 
     @ResponseBody
@@ -19,4 +19,5 @@ public class PostsViewController {
     public String single(@PathVariable("id") Long id) {
         return "Здесь будет страница поста №" + id;
     }
+
 }
