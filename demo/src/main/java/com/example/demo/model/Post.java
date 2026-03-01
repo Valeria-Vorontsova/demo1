f3a631036faefae749e9f5d8fbf29d040bddc8df
Валерия Воронцова <leraq8630@gmail.com>
2026-03-01 15:28:02 +0500

    bootstrap

--- demo/src/main/java/com/example/demo/model/Post.java
+++ demo/src/main/java/com/example/demo/model/Post.java
@@ -1,13 +1,19 @@
 package com.example.demo.model;
 
+import java.util.Date;
+
 public class Post {
     private String text;
     private Integer likes;
+    private Date creationDate;
 
     public Post (String text){
         this.text = text;
         this.likes = 0;
+        this.creationDate = new Date();
     }
+
+    public Date getCreationDate() { return creationDate; }
     public String getText() {
         return text;
     }
