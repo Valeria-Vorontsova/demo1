de27278b694edbe5aa54e457257551370912f004
Валерия Воронцова <leraq8630@gmail.com>
2026-03-01 20:59:18 +0500

    likes

--- demo/src/main/resources/static/main.js
+++ demo/src/main/resources/static/main.js
@@ -1,1 +1,12 @@
-alert("debug");
+$(document).ready(function(){
+    $('.like').click(function(e){
+        var btn = $(e.currentTarget);
+        var postId = btn.attr("data-post-id");
+        btn.attr("disabled", "disabled");
+        $.post("/post/" + postId + "/like", (data) => {
+            btn.text("" + data + " ❤");
+            btn.removeClass("btn-secondary");
+            btn.addClass("btn-danger");
+        });
+    });
+});
